390c749bd0522457a3fd395e3787226be78e3345
Majick333 <rmccauleycode@gmail.com>
2020-09-25 00:11:03 -0500

    9.24.20

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :notebooks
   has_many :notes, through: :notebooks
   has_many :calendars
-  has_many :events,through: :calendars
+  has_many :events, through: :calendars
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
